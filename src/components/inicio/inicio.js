41e8c352844230d705da8202ebd7d84e40bedf44
GabrielKraus <gabrielalvarezkraus@gmail.com>
2022-04-04 16:51:19 -0300

    Estilos modificados

--- src/components/inicio/inicio.js
+++ src/components/inicio/inicio.js
@@ -1,6 +1,23 @@
+import "./inicio.css"
+import  {Link } from "react-router-dom"
 const Inicio = () =>{
     return(
-        <h1>Inicio</h1>
+        <div className="inicio">
+            <div className="inicioOpciones">
+                <Link to="/productos" className="opcionesProductos inicioOpcion">
+                    <h2>TODOS LOS PRODUCTOS</h2>
+                </Link>
+                <Link to="/productos/categoria/bebidas" className="opcionesBebidas inicioOpcion">
+                    <h2>BEBIDAS</h2>
+                </Link>
+                <Link to="/productos/categoria/tabacos" className="opcionesTabaco inicioOpcion">
+                    <h2>TABACOS</h2>
+                </Link>
+            </div>
+            <div className="inicioDestacados">
+
+            </div>
+        </div>
     )
 }
 
--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -2,10 +2,12 @@ import Item from "../item/item";
 import "./itemList.css"
 
 const ItemList = (props) => {
-    return(
-        <ul className="itemList">
-            {props.products.map(product =><Item key={product.id} id={product.id} img={product.img} brand={product.brand} name={product.name} price={product.price} stock={product.stock}/>)}
-        </ul>
+    return (
+        <div className="productos">
+            <ul className="itemList">
+                {props.products.map(product => <Item key={product.id} id={product.id} img={product.img} brand={product.brand} name={product.name} price={product.price} stock={product.stock} />)}
+            </ul>
+        </div>
     )
 }
 
